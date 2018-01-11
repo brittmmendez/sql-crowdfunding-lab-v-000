62456e3dcda8261a2d3ed03e5a01acd125bbfc16
Brittany Morris <brittmmendez@gmail.com>
2018-01-11 00:20:21 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,9 +14,10 @@ GROUP BY(projects.title);"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name,users.age, pledges.amount FROM users
-JOIN pledges
+"SELECT users.name,users.age, SUM(pledges.amount) FROM users
+JOIN pledges 
 ON users.id = pledges.user_id
+GROUP BY (name)
 ORDER BY name;"
 end
 
